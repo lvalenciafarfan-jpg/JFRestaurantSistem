66b9a8d189928e0a3cb89aceacaf348d2c8a471d
Luis Valencia <lvalenciafarfan@gmail.com>
2026-09-04 16:43:36 -0500

    feat: implementar UserDetails en Usuario y UsuarioDetailsService

--- backend/src/main/java/com/lsvf/backend/entities/Usuario.java
+++ backend/src/main/java/com/lsvf/backend/entities/Usuario.java
@@ -6,14 +6,19 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +44,35 @@ public class Usuario {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private EstadoUsuario estadoCuenta;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(rolUsuario.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return correo;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return estadoCuenta != EstadoUsuario.BLOQUEADO;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return estadoCuenta == EstadoUsuario.ACTIVO;
+    }
+
 }
