891d1336ceb4995239bd83571dc826de6c7d8ae9
Luis Valencia <lvalenciafarfan@gmail.com>
2026-09-07 17:37:06 -0500

    feat: agregar dependencia validation y validaciones al productodto.

--- backend/src/main/java/com/lsvf/backend/dtos/producto/ProductoRequest.java
+++ backend/src/main/java/com/lsvf/backend/dtos/producto/ProductoRequest.java
@@ -1,8 +1,11 @@
 package com.lsvf.backend.dtos.producto;
 
 import com.lsvf.backend.enums.DisponibilidadProducto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 
@@ -10,13 +13,20 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ProductoRequest {
 
+    @NotBlank(message = "El nombre es obligatorio.")
+    @Length(max = 50, min = 5)
     private String nombre;
 
+    @NotBlank(message = "La descripcion es obligatoria.")
+    @Length(max = 255, min = 10)
     private String descripcion;
 
+    @NotNull(message = "El precio es obligatorio.")
     private BigDecimal precio;
 
+    @NotBlank(message = "La url de la imagen es obligaoria")
     private String urlImagen;
 
+    @NotBlank(message = "La disponibilidad es obligatoria.")
     private DisponibilidadProducto disponibilidadProducto;
 }
